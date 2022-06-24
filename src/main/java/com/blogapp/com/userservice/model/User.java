901b7a1462d670ee8754080834262ef8137b8456
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-06-23 21:17:46 -0500

    user and users model

--- src/main/java/com/blogapp/com/userservice/model/User.java
+++ src/main/java/com/blogapp/com/userservice/model/User.java
@@ -1,2 +1,39 @@
-package com.blogapp.com.userservice.model;public class User {
+package com.blogapp.com.userservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @NotNull
+    String fullName;
+    @NotNull
+    String userName;
+    @NotNull
+    @Email
+    String email;
+    @NotNull
+    @Min(8)
+    String password;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/blogapp/com/userservice/model/Users.java
+++ src/main/java/com/blogapp/com/userservice/model/Users.java
@@ -1,5 +1,19 @@
 package com.blogapp.com.userservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    Collection<User> users;
+
+    public int size(){
+        return this.users.size();
+    }
 
 }
