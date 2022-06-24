bc630918d2b404f5cff48191cb9673e3e9fd0cc3
Sagar Kunwar <sagar@Sagars-MacBook-Air.local>
2022-06-23 21:18:49 -0500

    update

--- src/main/java/com/blogapp/com/userservice/repository/UserRepository.java
+++ src/main/java/com/blogapp/com/userservice/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.blogapp.com.userservice.repository;public class UserRepository {
+package com.blogapp.com.userservice.repository;
+
+import com.blogapp.com.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
